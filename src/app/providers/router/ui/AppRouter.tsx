f70b3261fc697c2e1317f8d15221d2903989e863
Alex <alexseier12345@gmail.com>
2023-03-27 23:52:15 +0300

    updated loki, added protected routers

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,36 +1,52 @@
 import { getAuthUserData } from 'entities/User';
 import { AboutPage } from 'pages/About';
-import React, { memo, Suspense, useMemo } from 'react';
+import React, {
+    memo, Suspense, useCallback, useMemo,
+} from 'react';
 import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
-import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import {
+    AppRouterProps,
+    AppRouters,
+    routeConfig,
+} from 'shared/config/routeConfig/routeConfig';
 import { PageLoader } from 'shared/ui/PageLoader/PageLoader';
+import { RequireAuth } from './RequireAuth';
 
 export const AppRouter = memo(() => {
     const isAuth = useSelector(getAuthUserData);
 
     const routers = useMemo(
-        () => Object.values(routeConfig).filter(
-            (route) => {
-                if (!route.authOnly) {
-                    return true;
-                }
-                return route.authOnly === !!isAuth;
-            },
-        ),
+        () => Object.values(routeConfig).filter((route) => {
+            if (!route.authOnly) {
+                return true;
+            }
+            return route.authOnly === !!isAuth;
+        }),
         [isAuth],
     );
+
+    const renderWithWrapper = useCallback((route: AppRouterProps) => {
+        const element = <div className="page-wrapper">{route.element}</div>;
+
+        return (
+            <Route
+                key={route.path}
+                path={route.path}
+                element={
+                    route.authOnly ? (
+                        <RequireAuth>{element}</RequireAuth>
+                    ) : (
+                        element
+                    )
+                }
+            />
+        );
+    }, []);
+
     return (
         <Suspense fallback={<PageLoader />}>
-            <Routes>
-                {routers.map(({ path, element }) => (
-                    <Route
-                        key={path}
-                        path={path}
-                        element={<div className="page-wrapper">{element}</div>}
-                    />
-                ))}
-            </Routes>
+            <Routes>{Object.values(routeConfig).map(renderWithWrapper)}</Routes>
         </Suspense>
     );
 });
--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,3 +1,4 @@
 export { userReducer, userActions } from './model/slice/userSlice';
 export { User, UserSchema } from './model/types/user';
 export { getAuthUserData } from './model/selectors/getAuthUserData/getAuthUserData';
+export { getUserInited } from './model/selectors/getUserInited/getUserInited';
